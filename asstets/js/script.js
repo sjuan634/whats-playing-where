2914e164432311ace781029300023087ce89b5f8
sanchezju <sjuan634@yahoo.com>
2022-11-21 20:09:26 -0800

    add js code for fetch

--- asstets/js/script.js
+++ asstets/js/script.js
@@ -1,29 +1,35 @@
 // http://api.fandango.com/<version>?op=<operation>&<parameter list>&apikey=<apikey>&sig=<sig>
-const fandangoAPIkey = 'y8xzwnm8dc9mh2dynbjx43d3';
-const FandangoSecretKey = 'c3U5keUBDh';
+// const fandangoAPIkey = 'y8xzwnm8dc9mh2dynbjx43d3';
+// const FandangoSecretKey = 'c3U5keUBDh';
 
-async function generateAPISig() {
-const unixTimestamp = Date.now ();
-const unhashedSig = fandangoAPIkey + FandangoSecretKey + unixTimestamp;
+// async function generateAPISig() {
+// const unixTimestamp = Date.now ();
+// const unhashedSig = fandangoAPIkey + FandangoSecretKey + unixTimestamp;
 
-var textAsBuffer = new TextEncoder().encode(unhashedSig);
-var hashBuffer = await window.crypto.subtle.digest('SHA-256', textAsBuffer);
-var hashArray = Array.from(new Uint8Array(hashBuffer))
-var hashedSig = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-return hashedSig;
+// var textAsBuffer = new TextEncoder().encode(unhashedSig);
+// var hashBuffer = await window.crypto.subtle.digest('SHA-256', textAsBuffer);
+// var hashArray = Array.from(new Uint8Array(hashBuffer))
+// var hashedSig = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+// return hashedSig;
+// }
+
+// function fandangoAPICall() {
+//   const hashedSig = generateAPISig();
+//   const apiURL = `http://api.fandango.com/v1/?op=theatersbypostalcodesearch&postalcode=94105&apikey=${fandangoAPIkey}&sig=${hashedSig}`;
+
+//   fetch(apiURL)
+//     .then(function (response) {
+//     return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data)      
+//     });
+//   }
+
+//   fandangoAPICall()
+
+const baseEndpoint = 'https://api-gate2.movieglu.com/';
+
+async function fetchMovieGluData(query) {
+  const response = await fetch(`${baseEndpoint}?q=${query}`)
 }
-
-function fandangoAPICall() {
-  const hashedSig = generateAPISig();
-  const apiURL = `http://api.fandango.com/v1/?op=theatersbypostalcodesearch&postalcode=94105&apikey=${fandangoAPIkey}&sig=${hashedSig}`;
-
-  fetch(apiURL)
-    .then(function (response) {
-    return response.json();
-    })
-    .then(function (data) {
-      console.log(data)      
-    });
-  }
-
-  fandangoAPICall()
